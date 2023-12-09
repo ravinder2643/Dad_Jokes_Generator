b5d8d9d2fb296b73fa60a598b8f77c48da774a8a
ravinder2643 <138236210+ravinder2643@users.noreply.github.com>
2023-12-09 09:46:55 +0530

    2nd  commit

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 console.log("hii ravi");
+
+const btnE1 = document.getElementById("btn")
+const jokeE1 = document.getElementById("joke")
+const apikey = "HU9r+71siCAHYgWTwzEL7g==pac77EBh7Tta2t7t";
+const apiURL= "https://api.api-ninjas.com/v1/dadjokes?limit=1"
+const option= {
+    method: "GET",
+    headers: {
+       "X-Api-Key" : apikey,
+    },
+};
+async function getjoke(){
+
+
+try {
+    jokeE1.innerText="Updating....";
+    btnE1.disabled =  true;
+    btnE1.innerText= "Loading......";
+        const response = await fetch(apiURL, option)
+    const data = await response.json()
+
+    btnE1.disabled =  false;
+    btnE1.innerText= "Tell me a joke";
+    jokeE1.innerText = data[0].joke;
+
+} catch (error) {
+    console.log(error);
+    
+}
+}
+
+
+
+btnE1.addEventListener("click", getjoke)
